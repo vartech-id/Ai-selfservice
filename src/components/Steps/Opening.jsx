1edaa2bebd1326ae3c98154b73b91c2aac8387f2
PvtMilo <flip360.id.solo.@gmail.com>
2025-10-20 20:04:50 +0700

    opening logo and next button

--- src/components/Steps/Opening.jsx
+++ src/components/Steps/Opening.jsx
@@ -1,18 +1,22 @@
 import tap from "../../assets/ui/tap.png";
 
-     const Opening = ({ onStart }) => {
-       return (
-         <div className="relative w-full h-full">
-           <img src={tap} alt="image/png" className="w-full h-full object-cover" />
-           <button
-             onClick={onStart}
-             className="absolute bottom-0 left-1/2 transform -translate-x-1/2 px-6 py-2 bg-[#BF9A30] px-14 rounded-full uppercase font-bold text-white
-      hover:bg-blue-600 transition-colors z-10"
-          >
-            Start
-          </button>
-        </div>
-      );
-    };
+const Opening = ({ onStart }) => {
+  return (
+    <div className="relative w-full h-full">
+      <img
+        src={tap}
+        alt="image/png"
+        className="w-full h-full object-cover object-top"
+      />
+      <p className="bg-white">Tap to start</p>
+      <button
+        onClick={onStart}
+        className="absolute bottom-[-35%] left-1/2 transform -translate-x-1/2 px-8 py-4 bg-[#BF9A30] px-16 rounded-full uppercase font-bold text-5xl text-white hover:bg-blue-600 transition-colors z-10"
+      >
+        NEXT →
+      </button>
+    </div>
+  );
+};
 
 export default Opening;
