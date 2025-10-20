55c13b892cc25ad3510343ef4907639634e378c3
PvtMilo <flip360.id.solo.@gmail.com>
2025-10-20 20:38:59 +0700

    opening layout no font

--- src/components/Steps/Opening.jsx
+++ src/components/Steps/Opening.jsx
@@ -8,10 +8,11 @@ const Opening = ({ onStart }) => {
         alt="image/png"
         className="w-full h-full object-cover object-top"
       />
-      <p className="bg-white">Tap to start</p>
+      <p>BUAT KARAKTER
+ALERTGEN KAMU!</p>
       <button
         onClick={onStart}
-        className="absolute bottom-[-35%] left-1/2 transform -translate-x-1/2 px-8 py-4 bg-[#BF9A30] px-16 rounded-full uppercase font-bold text-5xl text-white hover:bg-blue-600 transition-colors z-10"
+        className="absolute bottom-[-55%] left-1/2 transform -translate-x-1/2 px-8 py-4 bg-[#BF9A30] px-16 rounded-full uppercase font-bold text-4xl text-white hover:bg-blue-600 transition-colors z-10"
       >
         NEXT →
       </button>
